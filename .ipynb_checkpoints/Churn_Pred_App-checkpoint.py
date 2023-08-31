247674b20114b2037ecf6aeeedd16af234240d38
Abir Maiti <abirmaiti56@gmail.com>
2023-08-31 23:52:14 +0530

    updating the app

--- .ipynb_checkpoints/Churn_Pred_App-checkpoint.py
+++ .ipynb_checkpoints/Churn_Pred_App-checkpoint.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.models import load_model
